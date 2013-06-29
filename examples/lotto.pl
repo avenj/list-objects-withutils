924b19056457c751d9a1ed0589ea81bca85ab3b9
Jon Portnoy <avenj@cobaltirc.org>
2013-06-29 03:36:28 -0400

    examples/ improvements

--- examples/lotto.pl
+++ examples/lotto.pl
@@ -1,7 +1,41 @@
 #!/usr/bin/env perl
 use strict; use warnings;
-
 use Lowu;
+
+my $selected = array;
+
+while ($selected->count < 6) {
+
+  if ($selected->count == 5) {
+    print " > Selected balls: ", $selected->join('-'), "\n";
+    my $eball;
+    do { 
+      print "Select an extra ball between 1 and 35: ";
+      $eball = <STDIN>;
+      chomp $eball;
+    } until $eball and $eball > 0 and $eball < 35;
+
+    $selected->push($eball);
+
+    last
+  }
+
+  my $ball;
+  do {
+    my $current = $selected->count;
+    print " > $current balls selected: ",
+          $selected->join('-'), "\n";
+    print "Select a ball between 1 and 59: ";
+    $ball = <STDIN>;
+    chomp $ball;
+  } until $ball and $ball > 0 and $ball < 59;
+
+  $selected->push($ball);
+}
+
+print " > You selected ", $selected->sliced(0 .. 4)->join('-'), "\n",
+      " > Extra ball ".$selected->get(5), "\n";
+
 
 my $balls = array( 1 .. 59 )
   ->shuffle
@@ -11,4 +45,24 @@ my $extra = array( 1 .. 35 )
   ->shuffle
   ->get(0);
 
-print $balls->join('-'), ' ', $extra, "\n";
+print "! Drew: ", $balls->join('-'), ' ', $extra, "\n";
+
+my $hits = $selected->sliced(0 .. 4)->grep(sub {
+  my $num = $_[0];
+  $balls->has_any(sub { $_ == $num })
+});
+
+my $did_hit;
+
+if ($hits->has_any) {
+  print "!! You hit on ", $hits->count, " balls:\n";
+  print "!!  ", $hits->join(', '), "\n";
+  ++$did_hit
+}
+
+if ($selected->get(0) == $extra) {
+  print "!! You hit on the extra ball ($extra)!\n";
+  ++$did_hit
+}
+
+print " >> Better luck next time :(\n" unless $did_hit;
