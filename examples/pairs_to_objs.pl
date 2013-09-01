59996bb6925c63d082c67c24132f91fe3c094a75
Jon Portnoy <avenj@cobaltirc.org>
2013-09-01 06:47:11 -0400

    examples/; add Types example

--- examples/pairs_to_objs.pl
+++ examples/pairs_to_objs.pl
@@ -2,7 +2,9 @@ use strict; use warnings;
 use Lowu;
 use Data::Dumper;
 
+use Types::Standard 'Str';
+
 print Dumper 
   [foo => 'bar', baz => 'quux']
-    ->tuples
+    ->tuples(2 => Str)
     ->map(sub { hash(@$_)->inflate })
