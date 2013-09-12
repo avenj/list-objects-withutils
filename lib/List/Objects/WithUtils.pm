64ea132f28267195305b7fc0c6b49ceb7ef14a45
Jon Portnoy <avenj@cobaltirc.org>
2013-09-12 15:32:42 -0400

    POD grammar tweak

--- lib/List/Objects/WithUtils.pm
+++ lib/List/Objects/WithUtils.pm
@@ -256,7 +256,7 @@ B<Why another object-oriented list module?>
 
 There are a fair few object-oriented approaches to lists on CPAN, none of
 which were quite what I needed. L<Data::Perl> comes the closest -- but is
-primarily targetting MooX::HandlesVia and cannot guarantee a stable API at the
+primarily targetting MooX::HandlesVia and could not guarantee a stable API at the
 time this was written (plus, I don't need the other data types).
 
 This module aims to provide a consistent, natural interface to hashes and
