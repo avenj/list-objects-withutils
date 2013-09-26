55901ca9a9e9493fd1302a417c01b74da6a33be8
Jon Portnoy <avenj@cobaltirc.org>
2013-09-26 13:00:10 -0400

    Add Hash::Immutable, Role::Hash::Immutable

---
+++ lib/List/Objects/WithUtils/Role/Hash/Immutable.pm
@@ -0,0 +1,36 @@
+package List::Objects::WithUtils::Role::Hash::Immutable;
+use strictures 1;
+use Carp ();
+
+sub _make_unimp {
+  my ($method) = @_;
+  sub {
+    local $Carp::CarpLevel = 1;
+    Carp::croak "Method '$method' not implemented on immutable hashes"
+  }
+}
+
+our @ImmutableMethods = qw/
+  clear
+  set
+  delete
+/;
+
+use Role::Tiny;
+requires 'new', @ImmutableMethods;
+
+use namespace::clean;
+
+around new => sub {
+  my $orig = shift;
+  my $self = $orig->(@_);
+
+  &Internals::SvREADONLY($self, 1);
+  Internals::SvREADONLY($_, 1) for values %$self;
+
+  $self
+};
+
+around $_ => _make_unimp($_) for @ImmutableMethods;
+
+1;
