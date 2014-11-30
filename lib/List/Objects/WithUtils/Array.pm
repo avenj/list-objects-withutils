4b66c53a88b9488b2b5ee0a4ae35f5216db08926
Jon Portnoy <avenj@cobaltirc.org>
2014-11-30 05:46:44 -0500

    touch a/b in caller when importing Array

--- lib/List/Objects/WithUtils/Array.pm
+++ lib/List/Objects/WithUtils/Array.pm
@@ -10,8 +10,18 @@ Role::Tiny->apply_roles_to_package( __PACKAGE__,
    /
 );
 
-use Exporter 'import';
+use Exporter ();
 our @EXPORT = 'array';
+
+sub import {
+  my $pkg = caller;
+  { no strict 'refs';
+    ${"${pkg}::a"} = ${"${pkg}::a"};
+    ${"${pkg}::b"} = ${"${pkg}::b"};
+  }
+  goto &Exporter::import
+}
+
 sub array { __PACKAGE__->new(@_) }
 
 1;
