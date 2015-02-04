f7a0264a018c3930ad1addd9073bc7844a045fb1
Jon Portnoy <avenj@cobaltirc.org>
2015-02-04 04:19:55 -0500

    return () rather than undef in hash->get_path

--- lib/List/Objects/WithUtils/Role/Hash.pm
+++ lib/List/Objects/WithUtils/Role/Hash.pm
@@ -97,7 +97,7 @@ sub get_path {
   my $ref = $_[0];
   for my $part (@_[1 .. $#_]) {
     $ref = ref $part eq 'ARRAY' ? $ref->[ $part->[0] ] : $ref->{$part};
-    return undef unless defined $ref;
+    return unless defined $ref;
   }
   $ref
 }
